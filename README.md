fb86b36c8592a570498ec4393e3d5350985d53b7
James Reeves <jreeves@weavejester.com>
2017-04-17 19:12:40 +0100

    Release 0.1.1

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ ClojureScript for a production release, use [compiler.cljs][].
 
 To install, add the following to your project `:dependencies`:
 
-    [duct/server.figwheel "0.1.0"]
+    [duct/server.figwheel "0.1.1"]
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/server.figwheel "0.1.0"
+(defproject duct/server.figwheel "0.1.1"
   :description "Integrant methods for running Figwheel"
   :url "https://github.com/duct-framework/server.figwheel"
   :license {:name "Eclipse Public License"

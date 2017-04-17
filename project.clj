882dbc3d410effb285b9a1afd48ccd047d51904f
James Reeves <jreeves@weavejester.com>
2017-04-17 17:46:08 +0100

    Fix indirect dependencies to Ring 1.5

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cemerick/piggieback "0.2.1"]
+                 [ring/ring-core "1.6.0-RC2"]
                  [figwheel-sidecar "0.5.8"]
                  [http-kit "2.2.0"]
                  [integrant "0.3.3"]
@@ -14,7 +15,7 @@
    :dev {:source-paths   ["dev/src/clj"]
          :resource-paths ["dev/resources" "target/js"]
          :dependencies [[duct/server.http.jetty "0.1.0"]
-                        [compojure "1.5.1"]
+                        [compojure "1.6.0-beta3"]
                         [figwheel "0.5.8"]
                         [org.clojure/tools.nrepl "0.2.12"]]
          :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})

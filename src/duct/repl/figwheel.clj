60e10389c95028e6a02652f11d7fc1378b6d41be
James Reeves <jreeves@weavejester.com>
2017-04-17 17:47:15 +0100

    Fix duct.repl.figwheel requires

--- src/duct/repl/figwheel.clj
+++ src/duct/repl/figwheel.clj
@@ -1,6 +1,6 @@
 (ns duct.repl.figwheel
-  (:require [integrant.repl :as repl]
+  (:require [integrant.repl.state :as state]
             [duct.server.figwheel :as figwheel]))
 
 (defn cljs-repl []
-  (figwheel/cljs-repl (:duct.server/figwheel repl/system)))
+  (figwheel/cljs-repl (:duct.server/figwheel state/system)))

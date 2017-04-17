b656192026bd1c111c22b394e549f91d7b72cb72
James Reeves <jreeves@weavejester.com>
2017-04-17 16:08:23 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/server.figwheel "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for running Figwheel"
+  :url "https://github.com/duct-framework/server.figwheel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]

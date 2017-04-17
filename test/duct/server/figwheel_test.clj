f68a850492e0ae2f47fe06773607eb921bd1707b
James Reeves <jreeves@weavejester.com>
2017-04-17 17:52:25 +0100

    Remove failing placeholder test

--- test/duct/server/figwheel_test.clj
+++
@@ -1,7 +0,0 @@
-(ns duct.server.figwheel-test
-  (:require [clojure.test :refer :all]
-            [duct.server.figwheel :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

8e49fbda5a7faa4b2f27bc6108eda509ed151dd4
James Reeves <jreeves@weavejester.com>
2017-05-31 22:34:37 +0100

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/server.figwheel "0.1.1"
+(defproject duct/server.figwheel "0.1.2"
   :description "Integrant methods for running Figwheel"
   :url "https://github.com/duct-framework/server.figwheel"
   :license {:name "Eclipse Public License"

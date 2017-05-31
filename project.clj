127aa0bd57b38fadf97f10ed58056f52cdfb2b7b
James Reeves <jreeves@weavejester.com>
2017-05-31 22:33:53 +0100

    Update Clojure and ClojureScript versions

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/duct-framework/server.figwheel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/core.async "0.3.443"]
                  [com.cemerick/piggieback "0.2.1"]
                  [compojure "1.6.0"]
                  [figwheel-sidecar "0.5.8"]
@@ -12,7 +13,7 @@
                  [integrant/repl "0.2.0"]
                  [ring/ring-core "1.6.1"]]
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "1.9.521"]]}
+  {:provided {:dependencies [[org.clojure/clojurescript "1.9.562"]]}
    :dev {:source-paths   ["dev/src/clj"]
          :resource-paths ["dev/resources" "target/js"]
          :dependencies [[duct/server.http.jetty "0.1.0"]

e662d70b9c1e9d177c9aeafe09017e64b44cc0d1
James Reeves <jreeves@weavejester.com>
2017-08-22 15:33:24 +0100

    Update dependencies (except Figwheel)

--- project.clj
+++ project.clj
@@ -5,19 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/core.async "0.3.443"]
-                 [com.cemerick/piggieback "0.2.1"]
+                 [com.cemerick/piggieback "0.2.2"]
                  [compojure "1.6.0"]
                  [duct/core "0.6.0"]
                  [figwheel-sidecar "0.5.8"]
                  [http-kit "2.2.0"]
                  [integrant "0.6.1"]
                  [integrant/repl "0.2.0"]
-                 [ring/ring-core "1.6.1"]]
+                 [ring/ring-core "1.6.2"]]
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "1.9.562"]]}
+  {:provided {:dependencies [[org.clojure/clojurescript "1.9.908"]]}
    :dev {:source-paths   ["dev/src/clj"]
          :resource-paths ["dev/resources" "target/js"]
-         :dependencies [[duct/server.http.jetty "0.1.0"]
+         :dependencies [[duct/server.http.jetty "0.2.0"]
                         [figwheel "0.5.8"]
-                        [org.clojure/tools.nrepl "0.2.12"]]
+                        [org.clojure/tools.nrepl "0.2.13"]]
          :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})

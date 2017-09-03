5ce00e8ecafa7c8c9de22c5e4517671f07c23ca3
James Reeves <jreeves@weavejester.com>
2017-09-03 21:17:37 +0100

    Update Clojure version to 1.9.0-alpha19

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/server.figwheel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
                  [org.clojure/core.async "0.3.443"]
                  [com.cemerick/piggieback "0.2.2"]
                  [compojure "1.6.0"]
